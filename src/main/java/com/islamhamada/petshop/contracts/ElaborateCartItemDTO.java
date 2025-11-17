b72bf6bae34e4627a89b569e409c975df163b701
IslamHamada <Islamhamada222@gmail.com>
2025-11-17 14:35:21 +0100

    add ElaborateCartItemDTO
    joining CartItem with Product entities

---
+++ src/main/java/com/islamhamada/petshop/contracts/ElaborateCartItemDTO.java
@@ -0,0 +1,18 @@
+package com.islamhamada.petshop.contracts;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ElaborateCartItemDTO {
+    private long product_id;
+    private String product_name;
+    private double product_price;
+    private long cart_item_id;
+    private int cart_item_count;
+}
