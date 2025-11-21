126c44835efe4c7487ac91f3dc904add48d788e9
IslamHamada <Islamhamada222@gmail.com>
2025-11-21 21:54:24 +0100

    add product_image attribute to ElaborateCartItemDTO to display the image in the frontend

--- src/main/java/com/islamhamada/petshop/contracts/ElaborateCartItemDTO.java
+++ src/main/java/com/islamhamada/petshop/contracts/ElaborateCartItemDTO.java
@@ -13,6 +13,7 @@ public class ElaborateCartItemDTO {
     private long product_id;
     private String product_name;
     private double product_price;
+    private String product_image;
     private long cart_item_id;
     private int cart_item_count;
 }
