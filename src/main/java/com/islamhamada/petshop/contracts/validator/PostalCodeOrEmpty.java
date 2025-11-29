90c4828b08b111df279b395af2c843d6cc716b18
IslamHamada <Islamhamada222@gmail.com>
2025-11-29 23:56:50 +0100

    add validator PostalCodeOrEmpty

---
+++ src/main/java/com/islamhamada/petshop/contracts/validator/PostalCodeOrEmpty.java
@@ -0,0 +1,27 @@
+package com.islamhamada.petshop.contracts.validator;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = PostalCodeOrEmptyValidator.class)
+public @interface PostalCodeOrEmpty {
+    String message() default "add valid postal code or keep empty";
+}
+
+class PostalCodeOrEmptyValidator implements ConstraintValidator<PostalCodeOrEmpty, String> {
+
+    @Override
+    public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
+        if(s == null || s.isEmpty())
+            return true;
+        return s.matches("^[1-9]\\d{3,4}$");
+    }
+}
