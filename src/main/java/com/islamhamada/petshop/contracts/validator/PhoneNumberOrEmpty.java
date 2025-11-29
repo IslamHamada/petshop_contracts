b2cc96774a78d96cf9d7977e5c83e6f95d4590f1
IslamHamada <Islamhamada222@gmail.com>
2025-11-29 23:57:04 +0100

    add PhoneNumberOrEmpty

---
+++ src/main/java/com/islamhamada/petshop/contracts/validator/PhoneNumberOrEmpty.java
@@ -0,0 +1,27 @@
+package com.islamhamada.petshop.contracts.validator;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = PhoneNumberOrEmptyValidator.class)
+public @interface PhoneNumberOrEmpty {
+    String message() default "add valid phone number or keep empty";
+}
+
+class PhoneNumberOrEmptyValidator implements ConstraintValidator<PhoneNumberOrEmpty, String> {
+
+    @Override
+    public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
+        if(s == null || s.isEmpty())
+            return false;
+        return s.matches("(^\\+[1-9]\\d|0)\\d{7,10}$");
+    }
+}
