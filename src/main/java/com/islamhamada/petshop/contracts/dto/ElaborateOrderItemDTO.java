f4f5dd2eb01430e5c3bf8ea172282634a4230bde
IslamHamada <Islamhamada222@gmail.com>
2025-11-30 20:18:45 +0100

    add multiple validation annotations

--- src/main/java/com/islamhamada/petshop/contracts/dto/ElaborateOrderItemDTO.java
+++ src/main/java/com/islamhamada/petshop/contracts/dto/ElaborateOrderItemDTO.java
@@ -1,13 +1,19 @@
 package com.islamhamada.petshop.contracts.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
 public class ElaborateOrderItemDTO {
+    @Positive
     private long product_id;
+    @NotBlank
     private String product_name;
+    @Positive
     private double price;
+    @Positive
     private long count;
 }
