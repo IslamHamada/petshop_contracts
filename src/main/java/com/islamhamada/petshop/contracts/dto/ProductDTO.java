614d4c723deb1f75bcc72d9ae95841c052925bad
IslamHamada <Islamhamada222@gmail.com>
2025-11-30 20:21:01 +0100

    move class to dto package + add multiple validation annotations

--- src/main/java/com/islamhamada/petshop/contracts/dto/ProductDTO.java
+++ src/main/java/com/islamhamada/petshop/contracts/dto/ProductDTO.java
@@ -1,5 +1,8 @@
-package com.islamhamada.petshop.contracts;
+package com.islamhamada.petshop.contracts.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,12 +13,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProductDTO {
+    @Positive
     private long id;
 
+    @NotBlank
     private String name;
 
+    @Min(0)
     private int quantity;
 
+    @Positive
     private double price;
 
     private String description;
