124bf5770de1e89136dedf421382a9e046b3b466
IslamHamada <Islamhamada222@gmail.com>
2025-11-30 20:17:12 +0100

    add multiple validation annotations

--- src/main/java/com/islamhamada/petshop/contracts/dto/ElaborateOrderDTO.java
+++ src/main/java/com/islamhamada/petshop/contracts/dto/ElaborateOrderDTO.java
@@ -1,5 +1,10 @@
 package com.islamhamada.petshop.contracts.dto;
 
+import com.islamhamada.petshop.contracts.validator.PhoneNumberOrEmpty;
+import com.islamhamada.petshop.contracts.validator.PostalCodeOrEmpty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Data;
 
@@ -9,15 +14,26 @@ import java.util.List;
 @Data
 @Builder
 public class ElaborateOrderDTO {
+    @NotBlank
     private Instant time;
+    @Positive
     private double price;
+    @NotBlank
     private String firstName;
+    @NotBlank
     private String lastName;
+    @NotBlank @PhoneNumberOrEmpty
     private String phoneNumber;
+    @NotBlank
     private String country;
+    @NotBlank
     private String city;
+    @NotBlank
     private String street;
+    @NotBlank @PostalCodeOrEmpty
     private String postalCode;
+    @NotBlank
     private String houseNumber;
+    @NotBlank @Size(min = 1)
     private List<ElaborateOrderItemDTO> elaborateOrderItems;
 }
