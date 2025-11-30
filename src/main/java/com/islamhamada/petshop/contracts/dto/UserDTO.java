661187831965eb9f8fecf5584569911b48d19a90
IslamHamada <Islamhamada222@gmail.com>
2025-11-30 20:20:17 +0100

    add multiple validation annotations

--- src/main/java/com/islamhamada/petshop/contracts/dto/UserDTO.java
+++ src/main/java/com/islamhamada/petshop/contracts/dto/UserDTO.java
@@ -1,5 +1,9 @@
 package com.islamhamada.petshop.contracts.dto;
 
+import com.islamhamada.petshop.contracts.validator.PhoneNumberOrEmpty;
+import com.islamhamada.petshop.contracts.validator.PostalCodeOrEmpty;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,15 +16,20 @@ import java.time.Instant;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserDTO {
+    @NotBlank
     private String username;
+    @NotBlank @Email
     private String email;
     private String firstName;
     private String lastName;
     private String street;
     private String houseNumber;
     private String city;
+    @PostalCodeOrEmpty
     private String postalCode;
     private String country;
+    @NotBlank
     private Instant createdAt;
+    @PhoneNumberOrEmpty
     private String phoneNumber;
 }
