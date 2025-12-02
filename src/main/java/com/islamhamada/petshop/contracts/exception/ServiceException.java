b6c360531e30ebdfc8c0568cd4562be76180f29c
IslamHamada <Islamhamada222@gmail.com>
2025-12-02 18:41:11 +0100

    rename GeneralException to ServiceException

--- src/main/java/com/islamhamada/petshop/contracts/exception/ServiceException.java
+++ src/main/java/com/islamhamada/petshop/contracts/exception/ServiceException.java
@@ -3,12 +3,12 @@ package com.islamhamada.petshop.contracts.exception;
 import lombok.Data;
 
 @Data
-public abstract class GeneralException extends RuntimeException{
+public abstract class ServiceException extends RuntimeException{
     private String error_code;
 
-    protected GeneralException() {}
+    protected ServiceException() {}
 
-    protected GeneralException(String message, String error_code){
+    protected ServiceException(String message, String error_code){
         super(message);
         this.error_code = error_code;
     }
