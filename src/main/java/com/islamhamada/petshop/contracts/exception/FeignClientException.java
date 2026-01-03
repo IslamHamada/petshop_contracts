a694076b8a9ea9f2f4f536b265b420ad458588fe
IslamHamada <islamhamada222@gmail.com>
2026-01-03 17:59:27 +0100

    use HttpStatus instead of int and add the required dependency

--- src/main/java/com/islamhamada/petshop/contracts/exception/FeignClientException.java
+++ src/main/java/com/islamhamada/petshop/contracts/exception/FeignClientException.java
@@ -1,12 +1,13 @@
 package com.islamhamada.petshop.contracts.exception;
 
 import lombok.Data;
+import org.springframework.http.HttpStatus;
 
 @Data
 public class FeignClientException extends ServiceException {
-    private int httpStatus;
+    private HttpStatus httpStatus;
 
-    public FeignClientException(String message, String error_code, int httpStatus){
+    public FeignClientException(String message, String error_code, HttpStatus httpStatus){
         super(message, error_code);
         this.httpStatus = httpStatus;
     }
