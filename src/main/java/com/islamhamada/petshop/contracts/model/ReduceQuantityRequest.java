4d1ba023d04e668e1d5088d5e214f353e21be294
IslamHamada <islamhamada222@gmail.com>
2026-01-10 11:03:00 +0100

    add @NoArgsConstructor to prevent deserialization problems

--- src/main/java/com/islamhamada/petshop/contracts/model/ReduceQuantityRequest.java
+++ src/main/java/com/islamhamada/petshop/contracts/model/ReduceQuantityRequest.java
@@ -3,8 +3,10 @@ package com.islamhamada.petshop.contracts.model;
 import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class ReduceQuantityRequest {
     @Positive
