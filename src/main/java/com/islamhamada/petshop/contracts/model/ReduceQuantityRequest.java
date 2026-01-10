050edaa193513d2e4b9eedd2194e078dbf6be8e5
IslamHamada <islamhamada222@gmail.com>
2026-01-10 10:34:45 +0100

    move ReduceQuantityRequest to contracts from product service

---
+++ src/main/java/com/islamhamada/petshop/contracts/model/ReduceQuantityRequest.java
@@ -0,0 +1,12 @@
+package com.islamhamada.petshop.contracts.model;
+
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ReduceQuantityRequest {
+    @Positive
+    int amount;
+}
